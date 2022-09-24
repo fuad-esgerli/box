81bf693fe518a019562dc4babec2014f4f6dec48
fuad-esgerli <atbmonsuno@gmail.com>
2022-09-24 23:29:00 +0400

    box

---
+++ box.js
@@ -0,0 +1,25 @@
+const colors = ["blue", "yellow", "red", "PaleVioletred", "brown", "orange", "green", "pink", "purple"]
+const button = document.getElementById("button");
+
+const container = document.getElementById("container")
+
+button.addEventListener("click", randomRengliQutularyarat) 
+
+function randomRengliQutularyarat () {
+    const randomColor = colors[Math.floor(Math.random()* colors.length)]
+     const topPx = Math.floor(Math.random()*800)
+     const leftPx = Math.floor(Math.random()*1200)
+
+
+
+     const myDivEl = document.createElement("div")
+     myDivEl.style.width= "30px"
+     myDivEl.style.height= "30px"
+     myDivEl.style.backgroundColor=randomColor
+
+     myDivEl.style.position="absolute"
+     myDivEl.style.left = "${leftPx}"
+     myDivEl.style.top = "$topPx"
+
+     container.appendChild(myDivEl)
+}
