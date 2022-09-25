da3710db26ddc6e231dd437a26cbfd0ea115f55c
fuad-esgerli <atbmonsuno@gmail.com>
2022-09-25 18:58:31 +0400

    tr

--- box.js
+++ box.js
@@ -7,19 +7,21 @@ button.addEventListener("click", randomRengliQutularyarat)
 
 function randomRengliQutularyarat () {
     const randomColor = colors[Math.floor(Math.random()* colors.length)]
-     const topPx = Math.floor(Math.random()*800)
+    
+    const topPx = Math.floor(Math.random()*800)
+    
      const leftPx = Math.floor(Math.random()*1200)
 
      console.log(topPx, leftPx)
 
-     const myDivEl = document.createElement("div")
+     const myDivEl = document.createElement(`div`)
      myDivEl.style.width= "30px"
      myDivEl.style.height= "30px"
      myDivEl.style.backgroundColor=randomColor
 
      myDivEl.style.position="absolute"
-     myDivEl.style.left = "${leftPx}px"
-     myDivEl.style.top = "${topPx}px"
+     myDivEl.style.left = `${leftPx}px`
+     myDivEl.style.top = `${topPx}px`
 
      container.appendChild(myDivEl)
 }
